122089913bb7d94bb303879a7bc3c39dc75c137c
Juan Contreras <jcontreras417@unab.edu.co>
2025-04-04 09:59:19 -0500

    Avance de la clase- mitad del login

---
+++ app/src/main/java/com/Juandavidcontreras/ecommerceapp/LoginScreen.kt
@@ -0,0 +1,69 @@
+package com.Juandavidcontreras.ecommerceapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo_unab),
+                contentDescription = "Logo Unab",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = "Iniciar Sesión",
+                fontSize = 24.sp, // Corregido
+                color = Color(0xFFFF9900),
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Correo Electrónico")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
