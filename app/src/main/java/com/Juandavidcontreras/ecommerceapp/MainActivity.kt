feeffd1201c51fbd44d91551c435bd753894de98
Juan David Contreras <Jcontreras417@unab.edu.co>
2025-04-09 09:37:48 -0500

    Configuracion Navegacion

--- app/src/main/java/com/Juandavidcontreras/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/Juandavidcontreras/ecommerceapp/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.Juandavidcontreras.ecommerceapp.ui.theme.EcommerceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EcommerceAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                val myNavController = rememberNavController()
+                val mystartDestination = "login"
+                NavHost(
+                    navController = myNavController,
+                    startDestination = mystartDestination,
+                    modifier = Modifier.fillMaxSize()
+
+
+                ){
+                    composable("login"){
+                        LoginScreen()
+
+                    }
+                    composable("register") {
+                        RegisterScreen()
+                    }
+                }
+
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EcommerceAppTheme {
-        Greeting("Android")
-    }
-}
